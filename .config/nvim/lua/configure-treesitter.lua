e898a64a56a296c8c047570daed84b2ef98254b7
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-10-08 13:14:34 -0700

    chore: add custom treesitter highlighting

--- .config/nvim/lua/configure-treesitter.lua
+++ .config/nvim/lua/configure-treesitter.lua
@@ -1,6 +1,31 @@
 require'nvim-treesitter.configs'.setup{
 	ensure_installed = {"c", "bash", "python", "typescript", "lua" },
-	highlight = { enable = true },
+	highlight = {
+		enable = true,
+		custom_captures = {
+			-- ["variable.parameter"] = "TSParameter"
+
+			["variable.builtin"] = "GruvboxAqua",
+
+			-- this looks weird
+			["constructor"] = "GruvboxYellow",
+			["type"] = "GruvboxYellow",
+			["type.builtin"] = "GruvboxYellow",
+
+			["method"] = "GruvboxAqua",
+			["function"] = "GruvboxAqua",
+			-- a python builtin function 'chr' was in orange instead of aqua
+			["function.builtin"] = "GruvboxAqua",
+
+			-- ["include"] = "TSKeyword",
+			-- ["operator"] = "TSKeyword",
+			["keyword.operator"] = "TSKeyword",
+
+			["punctuation.bracket"] = "TSNone",
+			["punctuation.special"] = "TSNone",
+			["punctuation.delimiter"] = "TSNone"
+		}
+	},
 	refactor = {
 		smart_rename = {
 			enable = true,
