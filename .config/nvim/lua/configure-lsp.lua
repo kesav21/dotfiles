c9aee9813bd9f756816d74ea770d671c12342a1a
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2020-10-20 19:06:42 -0700

    chore: send ccls logs to /tmp

--- .config/nvim/lua/configure-lsp.lua
+++ .config/nvim/lua/configure-lsp.lua
@@ -1,2 +1,5 @@
 require'nvim_lsp'.tsserver.setup{}
-require'nvim_lsp'.ccls.setup{}
+require'nvim_lsp'.ccls.setup {
+	cmd = { "ccls", '--init={"cache": {"directory": "/tmp/ccls-cache"}}' }
+}
+require'nvim_lsp'.pyls.setup{}
