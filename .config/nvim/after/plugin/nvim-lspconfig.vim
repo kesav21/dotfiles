af77f1dd03cf58c4b55cc70e57d90281f966bc5a
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-08 09:15:39 -0700

    chore(nvim): map lsp bindings on attach

--- .config/nvim/after/plugin/nvim-lspconfig.vim
+++ .config/nvim/after/plugin/nvim-lspconfig.vim
@@ -1,19 +1,5 @@
 lua require('configure-lsp')
 
-nnoremap <silent> gd         <cmd>lua vim.lsp.buf.definition()<CR>
-nnoremap <silent> gi         <cmd>lua vim.lsp.buf.implementation()<CR>
-nnoremap <silent> gt         <cmd>lua vim.lsp.buf.type_definition()<CR>
-nnoremap <silent> gr         <cmd>lua vim.lsp.buf.references()<CR>
-nnoremap <silent> K          <cmd>lua vim.lsp.buf.hover()<CR>
-nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>
-nnoremap <silent> <leader>ca <cmd>lua vim.lsp.buf.code_action()<CR>
-inoremap <silent> <c-k>      <cmd>lua vim.lsp.buf.signature_help()<CR>
-
-autocmd Filetype python setlocal omnifunc=v:lua.vim.lsp.omnifunc
-
-setlocal completeopt-=preview
-
-" set omnifunc="v:lua.vim.lsp.omnifunc"
 " sign define LspDiagnosticsErrorSign       text=✖
 " sign define LspDiagnosticsWarningSign     text=⚠
 " sign define LspDiagnosticsInformationSign text=ℹ
--- .config/nvim/lua/configure-lsp.lua
+++ .config/nvim/lua/configure-lsp.lua
@@ -1,11 +1,31 @@
-require'lspconfig'.tsserver.setup {
-	-- TODO: fix; doesn't print kk
-	on_attach = function(client)
-		print('kk')
-	end
+local lspconfig = require('lspconfig')
+
+function on_attach(client)
+	print('attached lsp client')
+
+	vim.api.nvim_set_keymap('n', 'gd'        , '<cmd>lua vim.lsp.buf.definition()<CR>'     , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', 'gi'        , '<cmd>lua vim.lsp.buf.implementation()<CR>' , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', 'gt'        , '<cmd>lua vim.lsp.buf.type_definition()<CR>', {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', 'gr'        , '<cmd>lua vim.lsp.buf.references()<CR>'     , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', 'K'         , '<cmd>lua vim.lsp.buf.hover()<CR>'          , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>'         , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>'    , {noremap = true, silent = true})
+	vim.api.nvim_set_keymap('i', '<c-k>'     , '<cmd>lua vim.lsp.buf.signature_help()<CR>' , {noremap = true, silent = true})
+
+	vim.o.completeopt = "menuone,noinsert,noselect"
+	vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
+end
+
+lspconfig.tsserver.setup {
+	on_attach = on_attach
 }
-require'lspconfig'.ccls.setup {
-	cmd = { "ccls", '--init={"cache": {"directory": "/tmp/ccls-cache"}}' }
+lspconfig.ccls.setup {
+	cmd = { "ccls", '--init={"cache": {"directory": "/tmp/ccls-cache"}}' },
+	on_attach = on_attach
 }
-require'lspconfig'.pyls.setup{}
-require'lspconfig'.gopls.setup{}
+lspconfig.pyls.setup {
+	on_attach = on_attach
+}
+lspconfig.gopls.setup {
+	on_attach = on_attach
+}
