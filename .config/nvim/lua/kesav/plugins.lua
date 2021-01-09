b318bf67e6fec0954d00fca7bae8638d02e12d62
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-09 14:56:36 -0700

    chore(nvim): load neoformat on specific filetypes

--- .config/nvim/lua/kesav/plugins.lua
+++ .config/nvim/lua/kesav/plugins.lua
@@ -7,7 +7,10 @@ end
 
 vim.cmd [[ packadd packer.nvim ]]
 
+
 return require('packer').startup(function()
+	local neoformat_ft = { 'python', 'sh', 'haskell', 'typescript', 'javascript', 'go', 'java' }
+
 	use { 'wbthomason/packer.nvim',
 		opt = true }                    -- manage packer
 	use 'gruvbox-community/gruvbox'     -- gruvbox theme
@@ -19,7 +22,8 @@ return require('packer').startup(function()
 	use 'junegunn/vim-slash'            -- better searching
 	use 'tommcdo/vim-lion'              -- code align
 	use 'tpope/vim-repeat'              -- better repeating for plugins
-	use 'sbdchd/neoformat'              -- code formatter
+	use { 'sbdchd/neoformat',
+		ft = neoformat_ft }             -- code formatter
 	use 'kovetskiy/sxhkd-vim'           -- sxhkd highlighting
 	use { 'dkarter/bullets.vim',
 		ft = 'markdown' }               -- markdown bulleting
