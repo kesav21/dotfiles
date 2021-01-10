0be304c9c01086a017e0b2ab15fb9235f913ebb1
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-09 17:00:08 -0700

    chore(nvim): add lua language server

--- .config/nvim/lua/kesav/lsp.lua
+++ .config/nvim/lua/kesav/lsp.lua
@@ -34,3 +34,28 @@ lspconfig.pyls.setup {
 lspconfig.gopls.setup {
 	on_attach = on_attach
 }
+
+local sumneko_root_path = vim.fn.stdpath('cache') .. '/lspconfig/sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path .. "/bin/Linux/lua-language-server"
+
+lspconfig.sumneko_lua.setup {
+	cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+				path = vim.split(package.path, ';'),
+			},
+			diagnostics = {
+				globals = {'vim'},
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+				},
+			},
+		},
+	},
+	on_attach = on_attach
+}
