7884921023d1b5f05394c85b3c611f17d43734f2
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-14 09:37:11 -0700

    fix(nvim): open help to correct row

--- .config/nvim/lua/kesav/telescope-pickers.lua
+++ .config/nvim/lua/kesav/telescope-pickers.lua
@@ -7,8 +7,10 @@ function M.find_files()
 	require('telescope.builtin').find_files {
 		find_command = { 'fd', '--hidden', '--type', 'file', },
 		attach_mappings = function(prompt_bufnr)
-			actions.goto_file_selection_edit:replace(function()
-				actions.goto_file_selection_tabedit(prompt_bufnr)
+			actions._goto_file_selection:replace(function()
+				local selection = actions.get_selected_entry()
+				actions.close(prompt_bufnr)
+				vim.cmd('tabedit ' .. selection.value)
 			end)
 			return true
 		end,
@@ -17,9 +19,11 @@ end
 
 function M.help_tags()
 	require('telescope.builtin').help_tags {
-	  attach_mappings = function(prompt_bufnr)
-			actions.goto_file_selection_edit:replace(function()
-				actions.goto_file_selection_tabedit(prompt_bufnr)
+		attach_mappings = function(prompt_bufnr)
+			actions._goto_file_selection:replace(function()
+				local selection = actions.get_selected_entry()
+				actions.close(prompt_bufnr)
+				vim.cmd('tab help ' .. selection.value)
 			end)
 			return true
 		end,
