792cdd567529f7d8a9c728bb4b6dd513e0a5db9d
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-24 14:36:10 -0700

    chore(nvim): title help/terminal bufs in tabline

--- .config/nvim/lua/kesav/tabline.lua
+++ .config/nvim/lua/kesav/tabline.lua
@@ -1,5 +1,22 @@
+local Job = require('plenary.job')
 
-local function gethighlight(tab, current)
+local function os_execute_output(cmd)
+	local command = table.remove(cmd, 1)
+	return Job:new({ command = command, args = cmd }):sync()
+end
+
+local function firstline(filename)
+	local file = io.open(filename)
+	if file then
+		for line in io.lines(filename) do
+			io.close(file)
+			return line
+		end
+	end
+end
+
+
+local function tab_highlight(tab, current)
 	if tab == current then
 		return "%#TabSel#"
 	else
@@ -7,13 +24,50 @@ local function gethighlight(tab, current)
 	end
 end
 
-local function getname(tab)
-	local win       = vim.api.nvim_tabpage_get_win(tab)
-	local buf       = vim.api.nvim_win_get_buf(win)
-	local fullname  = vim.api.nvim_buf_get_name(buf)
-	local name      = vim.fn.fnamemodify(fullname, ":.")
-	local shortname = vim.fn.pathshorten(name)
-	return shortname
+local function buf_normal_title(buf)
+	local bufname = vim.api.nvim_buf_get_name(buf)
+	if bufname == "" then
+		return "[No Name]"
+	else
+		local filepath  = vim.fn.fnamemodify(bufname, ":.")
+		local shortname = vim.fn.pathshorten(filepath)
+		return shortname
+	end
+end
+
+local function buf_terminal_title(buf)
+	local id = vim.api.nvim_buf_get_var(buf, 'terminal_job_id')
+	local pid = vim.fn.jobpid(id)
+	local shell = firstline("/proc/" .. pid .."/cmdline")
+	local cwd_output = os_execute_output {
+		"readlink",
+		"-e",
+		"/proc/" .. pid .."/cwd",
+	}
+	local filepath  = vim.fn.fnamemodify(cwd_output[1], ":~")
+	return string.format("[%s] %s", shell, filepath)
+end
+
+local function buf_help_title(buf)
+	local bufname = vim.api.nvim_buf_get_name(buf)
+	local filename = vim.fn.fnamemodify(bufname, ":t")
+	return '[h] ' .. filename
+end
+
+local function tab_title(tab)
+	local win     = vim.api.nvim_tabpage_get_win(tab)
+	local buf     = vim.api.nvim_win_get_buf(win)
+	local buftype = vim.api.nvim_buf_get_option(buf, 'buftype')
+
+	if buftype == "" then
+		return buf_normal_title(buf)
+	elseif buftype == 'terminal' then
+		return buf_terminal_title(buf)
+	elseif buftype == 'help' then
+		return buf_help_title(buf)
+	else
+		return 'TODO: handle ' .. buftype
+	end
 end
 
 function _G.tabline()
@@ -26,7 +80,7 @@ function _G.tabline()
 
 	local line = ""
 	for _, tab in ipairs(tabs) do
-		line = line .. gethighlight(tab, current) .. " " .. getname(tab) .. " "
+		line = line .. tab_highlight(tab, current) .. " " .. tab_title(tab) .. " "
 	end
 	return line .. "%#TabClear#"
 end
