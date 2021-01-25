a772542caa3e2b503d24bbcb4fdf7b9c2b2032cb
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-24 18:36:07 -0700

    chore(nvim): get terminal cwd from /proc/pid/cwd

--- .config/nvim/lua/kesav/telescope-pickers.lua
+++ .config/nvim/lua/kesav/telescope-pickers.lua
@@ -4,6 +4,13 @@ local M = {}
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
+local Job = require('plenary.job')
+
+local function os_execute_output(cmd)
+	local command = table.remove(cmd, 1)
+	return Job:new({ command = command, args = cmd }):sync()
+end
+
 function M.find_files()
 	builtin.find_files {
 		find_command = { 'fd', '--hidden', '--type', 'file', },
@@ -33,25 +40,23 @@ function M.find_dirs()
 	}
 end
 
-local function first(iterator)
-	for i in iterator do
-		return i
-	end
-end
-
 function M.terminal_cd()
-	local line = vim.fn.getline('.')
-	local cwd = first(string.gmatch(line, '%[(.+)%]'))
-	local full_cwd = vim.fn.fnamemodify(cwd, ':p')
+	local id = vim.b.terminal_job_id
+	local pid = vim.fn.jobpid(id)
+	local cwd_output = os_execute_output {
+		"readlink",
+		"-e",
+		"/proc/" .. pid .."/cwd",
+	}
+	local cwd = cwd_output[1]
 
 	builtin.find_files {
 		prompt_title = 'Find Directories',
-		find_command = { 'fd', '--hidden', '--type', 'directory', '--base-directory', full_cwd },
+		find_command = { 'fd', '--hidden', '--type', 'directory', '--base-directory', cwd },
 		attach_mappings = function(prompt_bufnr)
 			actions._goto_file_selection:replace(function()
 				local selection = actions.get_selected_entry()
 				actions.close(prompt_bufnr)
-				local id = vim.b.terminal_job_id
 				vim.fn.chansend(id, 'cd ' .. selection.value .. '\n')
 				vim.fn.feedkeys('i')
 			end)
