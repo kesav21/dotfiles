24950e90a9f22f7811580806938744e725fb79fe
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-25 19:03:19 -0700

    chore(nvim): override _select

--- .config/nvim/lua/kesav/telescope-pickers.lua
+++ .config/nvim/lua/kesav/telescope-pickers.lua
@@ -4,18 +4,18 @@ local M = {}
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
-local Job = require('plenary.job')
+-- local Job = require('plenary.job')
 
-local function os_execute_output(cmd)
-	local command = table.remove(cmd, 1)
-	return Job:new({ command = command, args = cmd }):sync()
-end
+-- local function os_execute_output(cmd)
+-- 	local command = table.remove(cmd, 1)
+-- 	return Job:new({ command = command, args = cmd }):sync()
+-- end
 
 function M.find_files()
 	builtin.find_files {
 		find_command = { 'fd', '--hidden', '--type', 'file', },
 		attach_mappings = function(prompt_bufnr)
-			actions._goto_file_selection:replace(function()
+			actions._select:replace(function()
 				local selection = actions.get_selected_entry()
 				actions.close(prompt_bufnr)
 				vim.cmd('tabedit ' .. selection.value)
@@ -30,7 +30,7 @@ function M.find_dirs()
 		prompt_title = 'Find Directories',
 		find_command = { 'fd', '--hidden', '--type', 'directory', },
 		attach_mappings = function(prompt_bufnr)
-			actions._goto_file_selection:replace(function()
+			actions._select:replace(function()
 				local selection = actions.get_selected_entry()
 				actions.close(prompt_bufnr)
 				vim.cmd('cd ' .. selection.value)
@@ -68,7 +68,7 @@ end
 function M.help_tags()
 	builtin.help_tags {
 		attach_mappings = function(prompt_bufnr)
-			actions._goto_file_selection:replace(function()
+			actions._select:replace(function()
 				local selection = actions.get_selected_entry()
 				actions.close(prompt_bufnr)
 				vim.cmd('tab help ' .. selection.value)
