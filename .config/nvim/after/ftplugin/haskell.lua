3e05f86350293a93b01ac009f901667270f9555d
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-01-25 23:42:46 -0700

    chore(nvim): move format autocmd back to ftplugin

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -4,3 +4,8 @@ vim.bo.softtabstop = 2
 vim.bo.expandtab   = true
 
 vim.bo.makeprg = 'stack build --copy-bins'
+
+vim.cmd [[ augroup format ]]
+vim.cmd [[ autocmd! ]]
+vim.cmd [[ autocmd BufWritePre undojoin | Neoformat ]]
+vim.cmd [[ augroup END ]]
--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -1,8 +1,3 @@
-vim.bo.tabstop     = 2
-vim.bo.shiftwidth  = 2
-vim.bo.softtabstop = 2
-vim.bo.expandtab   = true
-
 vim.cmd [[ augroup format ]]
 vim.cmd [[ autocmd! ]]
 vim.cmd [[ autocmd BufWritePre undojoin | Neoformat ]]
