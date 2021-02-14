ec37c4a92beb8833e9962bb5b17994f6260452f7
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-02-14 00:49:07 -0700

    chore(nvim): remove terminal_cd

--- .config/nvim/lua/kesav/telescope-pickers.lua
+++ .config/nvim/lua/kesav/telescope-pickers.lua
@@ -7,13 +7,6 @@ local finders = require('telescope.finders')
 local sorters = require('telescope.sorters')
 local actions = require('telescope.actions')
 
--- local Job = require('plenary.job')
-
--- local function os_execute_output(cmd)
--- 	local command = table.remove(cmd, 1)
--- 	return Job:new({ command = command, args = cmd }):sync()
--- end
-
 function M.find_files()
 	builtin.find_files {
 		find_command = { 'fd', '--hidden', '--type', 'file', },
@@ -35,31 +28,6 @@ function M.find_dirs()
 		end,
 	}:find()
 end
-
--- function M.terminal_cd()
--- 	local id = vim.b.terminal_job_id
--- 	local pid = vim.fn.jobpid(id)
--- 	local cwd_output = os_execute_output {
--- 		"readlink",
--- 		"-e",
--- 		"/proc/" .. pid .."/cwd",
--- 	}
--- 	local cwd = cwd_output[1]
-
--- 	builtin.find_files {
--- 		prompt_title = 'Find Directories',
--- 		find_command = { 'fd', '--hidden', '--type', 'directory', '--base-directory', cwd },
--- 		attach_mappings = function(prompt_bufnr)
--- 			actions._goto_file_selection:replace(function()
--- 				local selection = actions.get_selected_entry()
--- 				actions.close(prompt_bufnr)
--- 				vim.fn.chansend(id, 'cd ' .. selection.value .. '\n')
--- 				vim.fn.feedkeys('i')
--- 			end)
--- 			return true
--- 		end,
--- 	}
--- end
 
 function M.buffers()
 	builtin.buffers { show_all_buffers = true }
