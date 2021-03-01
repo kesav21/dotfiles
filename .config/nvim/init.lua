8ae6b29b68da5bb09545649912221020a8f2a4a8
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-02-28 18:59:41 -0700

    chore: reorganize init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,6 +46,53 @@ vim.wo.list           = true
 vim.wo.colorcolumn    = '88'
 vim.wo.signcolumn     = 'yes'
 
+vim.cmd [[ augroup commentstrings ]]
+vim.cmd [[ autocmd! ]]
+vim.cmd [[ autocmd FileType c                lua vim.bo.commentstring = "// %s" ]]
+vim.cmd [[ autocmd FileType cpp              lua vim.bo.commentstring = "// %s" ]]
+vim.cmd [[ autocmd FileType javascript       lua vim.bo.commentstring = "// %s" ]]
+vim.cmd [[ autocmd FileType typescript       lua vim.bo.commentstring = "// %s" ]]
+vim.cmd [[ autocmd FileType typescript.react lua vim.bo.commentstring = "// %s" ]]
+vim.cmd [[ autocmd FileType asm              lua vim.bo.commentstring = "# %s" ]]
+vim.cmd [[ autocmd FileType crontab          lua vim.bo.commentstring = "# %s" ]]
+vim.cmd [[ autocmd FileType markdown         lua vim.bo.commentstring = "<!-- %s -->" ]]
+vim.cmd [[ autocmd FileType matlab           lua vim.bo.commentstring = "% %s" ]]
+vim.cmd [[ autocmd FileType xdefaults        lua vim.bo.commentstring = "! %s" ]]
+vim.cmd [[ augroup END ]]
+
+-- formatoptions
+-- t break the line if it gets too long
+-- c break the comment if it gets too long
+-- r continue comment after <enter>
+-- o continue comment after o or O
+-- q format comments?
+-- w something about trailing whitespace
+-- a auto-format paragraph
+-- ac auto-format commented paragraph
+-- n indent subsequent lines of numbered lists
+-- 2 indent subsequent lines to match indent of second line
+-- v break line only at blanks inserted during the current insert command
+-- b break line only at blanks inserted before the textwidth and during the current insert command
+-- l break line only if line was shorter than textwidth before the current insert command
+-- m break at weird characters
+-- M ???
+-- B ???
+-- 1 don't break after a one-letter word
+-- ] ???
+-- j remove comment leader when joining lines
+-- p don't break after period and single space
+
+vim.cmd [[ augroup buffer_local_options ]]
+vim.cmd [[ autocmd! ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.tabstop       = 4       ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.shiftwidth    = 4       ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.softtabstop   = 4       ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.expandtab     = false   ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.modeline      = true    ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.textwidth     = 88      ]]
+vim.cmd [[ autocmd FileType * lua vim.bo.formatoptions = 'crqj' ]] -- this doesn't work
+vim.cmd [[ augroup END ]]
+
 vim.cmd [[ filetype plugin indent on ]]
 vim.cmd [[ syntax on ]]
 
@@ -87,41 +134,6 @@ nnoremap { '<leader>cc', toggle_colorcolumn }
 
 --- }}}
 
--- autocommands {{{
-
--- formatoptions
--- t break the line if it gets too long
--- c break the comment if it gets too long
--- r continue comment after <enter>
--- o continue comment after o or O
--- q format comments?
--- w something about trailing whitespace
--- a auto-format paragraph
--- ac auto-format commented paragraph
--- n indent subsequent lines of numbered lists
--- 2 indent subsequent lines to match indent of second line
--- v break line only at blanks inserted during the current insert command
--- b break line only at blanks inserted before the textwidth and during the current insert command
--- l break line only if line was shorter than textwidth before the current insert command
--- m break at weird characters
--- M ???
--- B ???
--- 1 don't break after a one-letter word
--- ] ???
--- j remove comment leader when joining lines
--- p don't break after period and single space
-
-vim.cmd [[ augroup buffer_local_options ]]
-vim.cmd [[ autocmd! ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.tabstop       = 4       ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.shiftwidth    = 4       ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.softtabstop   = 4       ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.expandtab     = false   ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.modeline      = true    ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.textwidth     = 88      ]]
-vim.cmd [[ autocmd FileType * lua vim.bo.formatoptions = 'crqnj' ]]
-vim.cmd [[ augroup END ]]
-
 vim.cmd [[ augroup misc ]]
 vim.cmd [[ autocmd! ]]
 vim.cmd [[ autocmd BufWritePre * %s/\s\+$//e ]]
@@ -131,20 +143,4 @@ vim.cmd [[ autocmd BufWritePost *sxhkdrc !pkill -USR1 sxhkd ]]
 vim.cmd [[ autocmd TextYankPost * silent! lua require('vim.highlight').on_yank { higroup = 'Visual', timeout = 250 } ]]
 vim.cmd [[ autocmd BufWritePost plugins.lua PackerCompile ]]
 vim.cmd [[ augroup END ]]
-
-vim.cmd [[ augroup commentstrings ]]
-vim.cmd [[ autocmd! ]]
-vim.cmd [[ autocmd FileType c                lua vim.bo.commentstring = "// %s" ]]
-vim.cmd [[ autocmd FileType cpp              lua vim.bo.commentstring = "// %s" ]]
-vim.cmd [[ autocmd FileType javascript       lua vim.bo.commentstring = "// %s" ]]
-vim.cmd [[ autocmd FileType typescript       lua vim.bo.commentstring = "// %s" ]]
-vim.cmd [[ autocmd FileType typescript.react lua vim.bo.commentstring = "// %s" ]]
-vim.cmd [[ autocmd FileType asm              lua vim.bo.commentstring = "# %s" ]]
-vim.cmd [[ autocmd FileType crontab          lua vim.bo.commentstring = "# %s" ]]
-vim.cmd [[ autocmd FileType markdown         lua vim.bo.commentstring = "<!-- %s -->" ]]
-vim.cmd [[ autocmd FileType matlab           lua vim.bo.commentstring = "% %s" ]]
-vim.cmd [[ autocmd FileType xdefaults        lua vim.bo.commentstring = "! %s" ]]
-vim.cmd [[ augroup END ]]
-
--- }}}
 
