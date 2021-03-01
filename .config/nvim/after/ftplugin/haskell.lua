bae3d6da2381ca0c8e410e60c6294e4051174062
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-02-28 18:39:48 -0700

    fix: catch and handle undojoin error

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -7,5 +7,5 @@ vim.bo.makeprg = 'stack build --copy-bins'
 
 vim.cmd [[ augroup format ]]
 vim.cmd [[ autocmd! ]]
-vim.cmd [[ autocmd BufWritePre * undojoin | Neoformat ]]
+vim.cmd [[ autocmd BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry ]]
 vim.cmd [[ augroup END ]]
--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -5,5 +5,5 @@ vim.bo.expandtab   = true
 
 vim.cmd [[ augroup format ]]
 vim.cmd [[ autocmd! ]]
-vim.cmd [[ autocmd BufWritePre * undojoin | Neoformat ]]
+vim.cmd [[ autocmd BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry ]]
 vim.cmd [[ augroup END ]]
