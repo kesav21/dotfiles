8700598cbebfdd6bebd832a3f12c500bc065b225
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-27 19:57:46 -0700

    feat(nvim): convert some mappings into commands

--- .config/nvim/plugin/commands.lua
+++ .config/nvim/plugin/commands.lua
@@ -15,3 +15,34 @@ vim.cmd [[command Bd bp|bd#]]
 vim.cmd [[cnoreabbrev bd Bd]]
 
 vim.cmd [[cnoreabbrev tsp TSP]]
+
+function _G.kesav.newfile()
+	vim.api.nvim_feedkeys(
+		string.format(
+			":edit %s/",
+			vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h")
+		),
+		"n",
+		true
+	)
+end
+vim.cmd [[cnoreabbrev newf lua kesav.newfile()]]
+
+function _G.kesav.copyfile()
+	vim.cmd [[silent !xsel -ib < %]]
+end
+vim.cmd [[cnoreabbrev copyf lua kesav.copyfile()]]
+
+function _G.kesav.runfile()
+	vim.cmd [[!%:p]]
+end
+vim.cmd [[cnoreabbrev runf lua kesav.runfile()]]
+
+function _G.kesav.togglecolumns()
+	if vim.wo.colorcolumn == "" then
+		vim.wo.colorcolumn = "88"
+	else
+		vim.wo.colorcolumn = ""
+	end
+end
+vim.cmd [[cnoreabbrev cols lua kesav.togglecolumns()]]
--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -6,7 +6,6 @@ end
 local nnoremap = vim.keymap.nnoremap
 local inoremap = vim.keymap.inoremap
 local vnoremap = vim.keymap.vnoremap
-local tnoremap = vim.keymap.tnoremap
 local onoremap = vim.keymap.onoremap
 
 inoremap { 'jk', '<esc>' }
@@ -23,21 +22,3 @@ local function consistent_cw()
 	vim.fn.execute("normal! " .. vim.v.count1 .. "w")
 end
 onoremap { 'w', consistent_cw }
-
-nnoremap { '<leader>e' , ':e <c-r>=expand("%:p:h") . "/"<cr>' }
-nnoremap { '<leader>cp', ':silent !xsel -ib < %<cr>'              }
-
-nnoremap { '<leader>t'   , ':edit term://zsh<cr>:set hidden<cr>' }
-tnoremap { '<c-\\><c-\\>', '<c-\\><c-n>' }
-
-nnoremap { '<f1>', ':make<cr>' }
-nnoremap { '<f2>', ':!%:p<cr>' }
-
-local function toggle_colorcolumn()
-	if vim.wo.colorcolumn == "" then
-		vim.wo.colorcolumn = "88"
-	else
-		vim.wo.colorcolumn = ""
-	end
-end
-nnoremap { '<leader>cc', toggle_colorcolumn }
