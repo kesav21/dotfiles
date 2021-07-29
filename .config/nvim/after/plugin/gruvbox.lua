b9fb241fbf2e617fa54201aed588cafbeed7d706
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-29 10:55:04 -0700

    feat(nvim): configure gruvbox if installed

--- .config/nvim/after/plugin/gruvbox.lua
+++ .config/nvim/after/plugin/gruvbox.lua
@@ -2,7 +2,12 @@ vim.g.gruvbox_italic = 1
 vim.g.gruvbox_invert_selection = 0
 vim.g.gruvbox_contrast_dark = "hard"
 vim.g.gruvbox_contrast_light = "hard"
-vim.cmd [[colorscheme gruvbox]]
+
+local has_gruvbox, _ = pcall(vim.cmd, "colorscheme gruvbox")
+if not has_gruvbox then
+	print("after/plugin/gruvbox.lua: install gruvbox-community/gruvbox")
+	return
+end
 
 -- hi GruvboxRedUnderline    guifg=#fb4934 gui=none
 -- hi GruvboxYellowUnderline guifg=#fabd2f gui=none
