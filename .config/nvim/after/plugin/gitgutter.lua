2b0fbfe85d9995cb3b19024ae721359ab1994f3e
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-29 10:54:48 -0700

    feat(nvim): configure gitgutter if installed

--- .config/nvim/after/plugin/gitgutter.lua
+++ .config/nvim/after/plugin/gitgutter.lua
@@ -1,3 +1,8 @@
+if vim.fn.exists(":GitGutterSignsDisable") ~= 2 then
+	print("after/plugin/gitgutter.lua: install airblade/vim-gitgutter")
+	return
+end
+
 vim.g.gitgutter_enabled = 1
 vim.g.gitgutter_map_keys = 0
 
