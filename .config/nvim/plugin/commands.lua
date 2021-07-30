744b8586fd6c88e1de8b484e0c478593d97b47d9
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-29 17:01:26 -0700

    feat(nvim): bring back fugitive commit

--- .config/nvim/plugin/commands.lua
+++ .config/nvim/plugin/commands.lua
@@ -1,16 +1,5 @@
 _G.kesav = {}
 
-function _G.kesav.commit(message)
-	if string.len(message) <= 50 then
-		vim.cmd(string.format([[!git commit --gpg-sign --message '%s']], message))
-	else
-		-- TODO: open message in "$(git rev-parse --show-toplevel)"/.git/COMMIT_EDITMSG
-		print("commit message too long")
-	end
-end
-vim.cmd [[command -nargs=1 GitCommit lua kesav.commit("<args>")]]
-vim.cmd [[cnoreabbrev commit GitCommit]]
-
 vim.cmd [[command Bd bp|bd#]]
 vim.cmd [[cnoreabbrev bd Bd]]
 
