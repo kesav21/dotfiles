bdf1247d69f33bc4058296625f0bb36e0d455671
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-30 10:12:06 -0700

    feat(nvim): simplify mapping to exit terminal mode

--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -6,6 +6,7 @@ end
 local nnoremap = vim.keymap.nnoremap
 local inoremap = vim.keymap.inoremap
 local vnoremap = vim.keymap.vnoremap
+local tnoremap = vim.keymap.tnoremap
 local onoremap = vim.keymap.onoremap
 
 inoremap { 'jk', '<esc>' }
@@ -16,6 +17,8 @@ vnoremap { 'p' , '"_dP' }
 
 nnoremap { '<c-j>' , '<c-w>w' }
 nnoremap { '<c-k>' , '<c-w>W' }
+
+tnoremap { '<c-\\><c-\\>' , '<c-\\><c-n>' }
 
 -- make cw consistent with dw, yw, vw
 local function consistent_cw()
