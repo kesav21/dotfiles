6f1ba1ab7d2f86d1d7a6a7332b7e7b08dfc417d2
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-10-24 21:54:46 -0700

    fix(nvim): clean up more files when making pdfs

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,2 +1,3 @@
 vim.wo.wrap = false
-vim.bo.makeprg = "pdflatex -shell-escape %; rm %:r.aux %:r.log"
+vim.bo.makeprg =
+	"pdflatex -shell-escape % && rm -f %:r.aux %:r.log %:r.bcf %:r.out %:r.run.xml pdfa.xmpi"
